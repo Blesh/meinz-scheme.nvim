fbad96c1b6a422cb9b6ac2324fe1757162a65be8
Blesh <onurcakmak117@gmail.com>
2024-01-28 22:31:14 +0100

    feat: added support for treesitter 0.9.2

--- lua/meinz-scheme/plugins/treesitter.lua
+++ lua/meinz-scheme/plugins/treesitter.lua
@@ -2,18 +2,18 @@ local c = require 'meinz-scheme.colors'
 
 return {
     -- Text
-    ['@text'] = { link = 'Normal' }, -- For strings considered text in a markup language.
-    ['@text.literal'] = { link = 'String' },
-    ['@text.reference'] = { ctermfg = c.blue_01 },
-    ['@text.title'] = { link = 'Title' }, -- Text that is part of a title.
-    ['@text.uri'] = { underline = true, ctermfg = c.blue_01 }, -- Any URI like a link or email.
-    ['@text.underline'] = { underline = true }, -- For text to be represented with an underline.
-    ['@text.todo'] = { link = 'Todo' },
+    ['@markup'] = { link = 'Normal' }, -- For strings considered markup in a markup language.
+    ['@markup.raw'] = { link = 'String' },
+    ['@markup.link'] = { ctermfg = c.blue_01 },
+    ['@markup.heading'] = { link = 'Title' }, -- markup that is part of a title.
+    ['@markup.link.url'] = { underline = true, ctermfg = c.blue_01 }, -- Any URI like a link or email.
+    ['@markup.underline'] = { underline = true }, -- For markup to be represented with an underline.
+    ['@markup.todo'] = { link = 'Todo' },
 
-    ['@text.note'] = { ctermfg = c.magenta_01 },
-    ['@text.strong'] = { bold = true },
-    ['@text.emphasis'] = { italic = true }, -- For text to be represented with emphasis.
-    ['@text.strike'] = { strikethrough = true }, -- For strikethrough text.
+    ['@markup.note'] = { ctermfg = c.magenta_01 },
+    ['@markup.strong'] = { bold = true },
+    ['@markup.emphasis'] = { italic = true }, -- For markup to be represented with emphasis.
+    ['@markup.strike'] = { strikethrough = true }, -- For strikethrough markup.
 
     -- Miscs
     ['@comment'] = { link = 'Comment' },
@@ -23,25 +23,25 @@ return {
     ['@constant'] = { link = 'Constant' },
     ['@constant.builtin'] = { link = 'Constant' },
     ['@constant.macro'] = { link = 'Constant' },
-    ['@define'] = { link = 'Macro' },
+    ['@keyword.directive.define'] = { link = 'Macro' },
     ['@macro'] = { link = 'Macro' },
     ['@string'] = { link = 'String' }, -- For escape characters within a string.
     ['@string.escape'] = { fg = c.magenta_01 }, -- For escape characters within a string.
-    ['@string.special'] = { fg = c.magenta_01 }, -- For escape characters within a string.
+    ['@markup.link.label'] = { fg = c.magenta_01 }, -- For escape characters within a string.
     ['@character'] = { link = 'Character'},
     ['@character.special'] = { link = 'SpecialChar' },
     ['@number'] = { link = 'Constant' },
     ['@boolean'] = { link = 'Constant' },
-    ['@float'] = { link = 'Constant' },
+    ['@number.float'] = { link = 'Constant' },
 
     -- Functions
     ['@function'] = { link = 'Function' },
     ['@function.macro'] = { link = 'Macro' },
     -- should also be different color I think
     ['@function.builtin'] = { ctermfg = c.yellow_02 },
-    ['@parameter'] = { ctermfg = c.white_02 }, -- For parameters of a function.
-    ['@method'] = { ctermfg = c.cyan_01 },
-    ['@field'] = { ctermfg = c.cyan_01 },
+    ['@variable.parameter'] = { ctermfg = c.white_02 }, -- For parameters of a function.
+    ['@function.method'] = { ctermfg = c.cyan_01 },
+    ['@variable.member'] = { ctermfg = c.cyan_01 },
     ['@property'] = { ctermfg = c.cyan_01 },
     -- maybe 'function' or maybe having different color like type is better
     -- actually can also be somewhat reasonable since constructores are constructing 'types'
@@ -49,20 +49,20 @@ return {
 
 
     -- Keywords
-    ['@conditional'] = { ctermfg = c.red_01 },
-    ['@repeat'] = { ctermfg = c.red_01 },
+    ['@keyword.conditional'] = { ctermfg = c.red_01 },
+    ['@keyword.repeat'] = { ctermfg = c.red_01 },
     ['@label'] = { fg = c.blue_01 }, -- For labels: `label:` in C and `:label:` in Lua.
     ['@operator'] = { link = 'Operator' },
     ['@keyword'] = { link = 'Keyword' },
-    ['@exception'] = { link = 'Macro' },
+    ['@keyword.exception'] = { link = 'Macro' },
     ['@variable'] = { link = 'Identifier' }, -- Identifier for now
     ['@type'] = { link = 'Type' },
     ['@type.definition'] = { link = 'Type' },
-    ['@storageclass'] = { link = 'Keyword' },
+    ['@keyword.storage'] = { link = 'Keyword' },
     ['@namespace'] = { ctermfg = c.yellow_01 },
-    ['@include'] = { link = 'Macro' },
-    ['@preproc'] = { link = 'Macro' },
-    ['@debug'] = { link = 'Debug' },
+    ['@keyword.import'] = { link = 'Macro' },
+    ['@keyword.directive'] = { link = 'Macro' },
+    ['@keyword.debug'] = { link = 'Debug' },
     ['@tag'] = { link = 'Constant' }, -- Tags like html tag names.
 
 
@@ -79,14 +79,14 @@ return {
     ['@class'] = { link = 'Type' },
     ['@keyword.operator'] = { link = 'Keyword' },
     -- ['@method.call'] = { link = 'Function' },
-    ['@method.call'] = { ctermfg = c.cyan_01 },
+    ['@function.method.call'] = { ctermfg = c.cyan_01 },
     -- ['@function.call'] = { link = 'Function' },
     ['@function.call'] = { ctermfg = c.magenta_01 },
     ['@variable.builtin'] = { ctermfg = c.yellow_02 },
     ['@punctuation.delimiter'] = { ctermfg = c.white_02 }, -- For delimiters ie: `.`
     ['@punctuation.bracket'] = { ctermfg = c.white_02 }, -- For brackets and parens.
-    ['@string.regex'] = { fg = c.green_01 }, -- For regexes.
-    ['@punctuation.special'] = { link = 'Type' },
+    ['@string.regexp'] = { fg = c.green_01 }, -- For regexes.
+    ['@markup.list'] = { link = 'Type' },
     ['@type.qualifier'] = { link = 'Keyword' },
     ['@none'] = { link = 'None' },
 
